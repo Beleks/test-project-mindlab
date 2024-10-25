efba83ff8c7647def0c7e00f49e0a0e56f64fd0e
Aleksey Beletsky <aleksey.beletsky.av@gmail.com>
2024-10-25 16:52:21 +0800

    Remove unnecessary code

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,6 @@
-<script setup>
-// import MainTable from ''
-</script>
+<script setup></script>
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+<template></template>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+<style scoped></style>
 
-  <main>
-    <TheWelcome />
-  </main>
-</template>
-
-<style scoped>
-
-</style>
